3f4aaaf159ac5f616ca2cdda84123ab03a17aea9
Narcelio <narcelio.slima@gmail.com>
2024-01-28 21:31:41 -0300

    Criando variavel aleatória e inicializando em -1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let numeroSecreto = 0;
 let numTentatitvas = 0;
 let numerosSecretos = [];
 let posicaoAtual = 0;
+const variavelAleatoriA = -1;
 
 function geraNumerosSecretos(){
     numerosSecretos = [];
